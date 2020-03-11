07db8a747b26e0154318f306444d51e67ac8c26a
coolpurey <link2purshottam@gmail.com>
2020-03-11 23:37:03 +0545

    fixed earlier steps 3

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,9 +13,9 @@ public class Greeter {
   }
 
 /** 
-* @whoo hooo everyboy 
+* @param someone the name of a person
+ *@return greeting string
  */
-
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
